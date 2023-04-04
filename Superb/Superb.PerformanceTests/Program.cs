8e9b92e03282113512b6a370c069c83bc434ae22
drakon <drakon660@gmail.com>
2023-04-04 15:53:34 +0200

    performance tests

---
+++ Superb/Superb.PerformanceTests/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+using Superb.PerformanceTests;
+
+BenchmarkRunner.Run<SuperbBenchmark>();
+
+Console.WriteLine("Hello, World!");
+
