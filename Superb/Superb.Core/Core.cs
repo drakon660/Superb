be1e141238da0d45e2cf0a78f668e74a807fcb7f
drakon <drakon660@gmail.com>
2023-04-18 13:36:47 +0200

    also

---
+++ Superb/Superb.Core/Core.cs
@@ -0,0 +1,46 @@
+﻿namespace Superb.Core;
+
+public class Batman
+{
+    public decimal Price { get; set; }
+}
+
+public class SampleRequest
+{
+    public string Value { get; set; }
+    public SampleResponse Fake { get; set; }
+}
+
+public class SampleResponse
+{
+    public int Val { get; set; }
+    public Batman Batman { get; set; }
+    public DateTime Data { get; set; }
+
+    public Item[] Item { get; set; }
+}
+
+public class Item
+{
+    public string Value { get; set; }
+    public int Value2 { get; set; }
+    public decimal Value3 { get; set; }
+}
+
+public class FilterRequest
+{
+    public Filter[] Filters { get; set; }
+    public Item Item { get; set; }
+}
+
+public class Filter
+{
+    public int Value1 { get; set; }
+    public string Value2 { get; set; }
+    public MiniFilter[] Mini { get; set; }
+}
+
+public class MiniFilter
+{
+    public int TestValue { get; set; }
+}
